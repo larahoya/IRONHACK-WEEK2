8f1404de4d945865b5f5384f2b55236e0e3ec6d5
Lara Hoya <larascully@hotmail.com>
2015-11-02 12:57:02 +0100

    Fizzbuzz

--- test2.rb
+++ test2.rb
@@ -1,28 +1,9 @@
-require 'RSpec'
 
 class Fizzbuzz
 	def check number
+		return "vodafone" if number.to_s[0] == "2"
+		return "nike" if number % 7 == 0
 		result = (number % 3 == 0 ? "Fizz" : "") + (number % 5 == 0 ? "Buzz" : "")
-		result.empty? ? number.to_s : result
+		result.empty? ? number : result
 	end
-end
-
-
-RSpec.describe "Fizzbuzz test" do 
-	it "return n for n" do
-		expect(Fizzbuzz.new.check(1)).to eq("1")
-	end
-	
-	it "returns 'fizz' if n / 3 == 0" do
-		expect(Fizzbuzz.new.check(3)).to eq("Fizz")
-	end
-
-	it "returns 'buzz' if n / 5 == 0" do
-		expect(Fizzbuzz.new.check(5)).to eq("Buzz")
-	end
-
-	it "returns 'fizzbuzz' if n / 3 == 0 && n / 5 == 0" do
-		expect(Fizzbuzz.new.check(15)).to eq("FizzBuzz")
-	end
-
 end
